c679f861469175bee9a4d20ad0fb692eac786f60
intanayui <45117678+intanayui@users.noreply.github.com>
2020-10-26 17:22:31 +0700

    Pertemuan 3

---
+++ HelloITTelkomPurwokerto/app/src/main/java/com/intan_18102233/helloittelkompurwokerto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.intan_18102233.helloittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
